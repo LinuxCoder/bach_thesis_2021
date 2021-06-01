78aba1d89634b91b2ebb7d33306f42054a8dae56
Levon <levon12341@gmail.com>
2021-06-01 23:15:52 +0300

    third commit

--- gitint.sh
+++ gitint.sh
@@ -1,5 +1,5 @@
 git add .
-git commit -m "second commit"
+git commit -m "third commit"
 git branch -M main
 git remote add origin https://github.com/LinuxCoder/bach_thesis_2021.git
 git push -u origin main
